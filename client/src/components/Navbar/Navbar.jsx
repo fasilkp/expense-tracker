dcd168ad51d831d195584162815f0324e13ce932
fasilkp <fasilkp314@gmail.com>
2022-09-22 12:59:57 +0530

    Navbar updated

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,53 @@
 import React from 'react'
-import { BiHome, BiBookAdd,BiBarChartAlt2,BiUser, BiListUl } from "react-icons/bi";
+import { BiHome, BiBookAdd, BiUser, BiCollection} from "react-icons/bi";
+import { HiCollection, HiHome,HiUser } from "react-icons/hi";
+import { AiOutlinePieChart, AiFillPieChart} from "react-icons/ai";
 import './Navbar.css'
+import { useState } from 'react';
 function Navbar() {
+    const click={
+        home:false,
+        list:false,
+        chart:false,
+        user:false
+    }
+    const [clicked, setClicked]=useState(click)
   return (
     <div className='Navbar'>
-        <div className="nav-item nav-home">
-            <BiHome className='icon'/>
+        <div className="nav-item nav-home" 
+        onClick={()=>setClicked({...click, home:true})}>
+            {
+                clicked.home ?
+                <HiHome className='navClicked'/> :
+                <BiHome/>
+            }
         </div>
-        <div className="nav-item nav-expense">
-            <BiListUl />
+        <div className="nav-item nav-expense"
+        onClick={()=>setClicked({...click, list:true})}>
+            {
+                clicked.list?
+                <HiCollection className='navClicked'/> :
+                <BiCollection />
+            }
         </div>
         <div className="nav-item nav-add-expense">
             <BiBookAdd></BiBookAdd>
         </div>
-        <div className="nav-item nav-graph">
-            <BiBarChartAlt2/>
+        <div className="nav-item nav-graph"
+        onClick={()=>setClicked({...click, chart:true})}>
+            {
+                clicked.chart ?
+                <AiFillPieChart className='navClicked'/> :
+                <AiOutlinePieChart/>
+            }
         </div>
-        <div className="nav-item nav-profile">
-            <BiUser/>
+        <div className="nav-item nav-profile"
+        onClick={()=>setClicked({...click, user:true})}>
+            {
+                clicked.user ?
+                <HiUser className='navClicked'/> :
+                <BiUser/>
+            }
         </div>
     </div>
   )
